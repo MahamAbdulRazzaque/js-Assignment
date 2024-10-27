4691f6cffd5d2a24ddfdba3959d94d2031b89f68
Maham Abdul Razzaque <mahamabdulrazzaque@gmail.com>
2024-10-28 03:08:38 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+
+// let studentName = "MahaM AbduL Razzaque";
+// let age = 17;
+// let major = "Commerce";
+// let college = "Degree fOr Women College Nazimabad";
+
+// alert("Student Name : " + studentName);
+// alert("Age : " + age);
+// alert("Major : " + major);
+// alert("College : " + college);
+
+// EnD**************************************
+
+// // Declare a variable and assign your message
+// let alertMessage = "Hello...!!! My Self MahaM AbduL Razzaque."; 
+
+// alert(alertMessage);
+
+// EnD***************************************
+
+// Declare a variable called email and assign your Email Address
+// let email = "mahamabdulrazzaque@gmail.com"; 
+
+// let message = "My email address is : " + email;
+
+// alert(message);
+
+// EnD****************************************
+
+// Declare a variable called book and assign the value
+// let book = "A smarter way to learn JavaScript.";
+
+// let message = "I am trying tO learn frOm the bOok " + book;
+
+// alert(message);
+
+// EnD********************************************
+
+// alert("Yah..! I can write HTML content through JavaScript");
+
+// EnD********************************************
+
+// alert("🤍💁‍♀️✨💜");
+
+// EnD*********************************************
+
+
